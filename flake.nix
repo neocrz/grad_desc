9ad865c49532b12846e3c7d1f726719408be2e58
neocrz <neo59crz@gmail.com>
2024-11-22 19:51:25 -0300

    init repo

---
+++ flake.nix
@@ -0,0 +1,53 @@
+{
+  description = "A Nix-flake-based Python development environment";
+
+  inputs.nixpkgs.url = "pinned-nixpkgs";
+  
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = let
+          python = pkgs.python311.override {
+            self = python;
+            packageOverrides = pyfinal: pyprev: {
+              
+            };
+          };
+        in
+        pkgs.mkShell {
+          venvDir = ".venv";
+          packages = with pkgs; [ 
+            (python.withPackages (python-pkgs: with python-pkgs; [
+              jupyterlab
+              matplotlib
+              pandas
+              numpy
+              seaborn
+              scipy
+              scikit-learn
+              imbalanced-learn
+              xgboost
+              # libevdev
+              # nbconvert
+              # pandocfilters
+              pip
+              playwright
+              # pypandoc
+              venvShellHook
+              # tensorflow
+            ]))
+          ];
+
+          postVenvCreation = ''
+            unset SOURCE_DATE_EPOCH
+          '';
+        };
+      });
+    };
+}
